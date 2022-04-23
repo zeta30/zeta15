43349cac4fe72e3d1d3f663aed8623e7f29f9e3f
rockstarcuba <102777282+rockstarcuba@users.noreply.github.com>
2022-04-22 21:33:07 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '5153989700:AAGKKS7HQ0cVukh-n6fcQrH0JRm7ZrMvPQw'
+API_ID = 16536294
+API_HASH = '0c780f6db3124fafbdc3dcde437e6f21'
+
+
+# DATOS DEL MOODLE
+USUARIO = 'usuario'
+PASSWORD = 'contraseña'
+USUARIO_ID = 1487
+ZIP_MB = 399
+MOODLE_URL = 'moodle.uclv.edu.cu'
+OWNER = [1180073584]
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'gmail@gmail.com'
+PASS_MEGA = 'contraseña*'
